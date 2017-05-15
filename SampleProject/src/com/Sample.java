190c3af04ac6c555d4c8e72e222fa13b9c18fd26
Sujay121 <Sujay121@users.noreply.github.com>
2017-05-15 16:13:52 +0530

    Added logic to display integer

--- SampleProject/src/com/Sample.java
+++ SampleProject/src/com/Sample.java
@@ -4,10 +4,13 @@ public class Sample {
 
 	public Sample() {
 		// TODO Auto-generated constructor stub
+		int a = 10;
+		System.out.println(a);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Sample a=new Sample();
 
 	}
 
