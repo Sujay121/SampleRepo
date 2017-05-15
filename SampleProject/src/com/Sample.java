87a7830a935abc34fa3e6ad0a4cb4de3e7ba736b
Sujay121 <Sujay121@users.noreply.github.com>
2017-05-15 18:27:13 +0530

    Adding change to local bugfix branch

--- SampleProject/src/com/Sample.java
+++ SampleProject/src/com/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public Sample() {
 		// TODO Auto-generated constructor stub
 		int a = 10;
-		System.out.println(a);
+		//System.out.println(a);
 	}
 
 	public static void main(String[] args) {
