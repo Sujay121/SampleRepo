ae55adaabf2fe56b34c7b8af64bc1af9740caf5d
Sujay121 <Sujay121@users.noreply.github.com>
2017-05-15 18:07:07 +0530

    adding code in Enhancment

--- SampleProject/src/com/Sample.java
+++ SampleProject/src/com/Sample.java
@@ -5,7 +5,9 @@ public class Sample {
 	public Sample() {
 		// TODO Auto-generated constructor stub
 		int a = 10;
+		String b="TEST ABC";
 		System.out.println(a);
+		System.err.println(b);
 	}
 
 	public static void main(String[] args) {
