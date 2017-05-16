ca9261a25f3dd1d1419c48ea6cb29ef2d9b7f5a9
ab14052 <ab14052@W7x64-NET-239.udnbnor.net>
2017-05-16 15:36:37 +0530

    Adding Str in feature branch

--- SampleProject/src/com/Sample.java
+++ SampleProject/src/com/Sample.java
@@ -5,7 +5,9 @@ public class Sample {
 	public Sample() {
 		// TODO Auto-generated constructor stub
 		int a = 10;
-		//System.out.println(a);
+		String Str = "Hello World";
+		System.out.println(a);
+		System.out.println(Str);
 	}
 
 	public static void main(String[] args) {
